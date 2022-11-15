80a3cf9cb76dcc913108f647f7014d97467aed13
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2022-11-14 22:16:43 -0500

    Add randomization

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 from cairosvg import svg2png
 import os
+import random as r
+
+# COLORS
+# white, black, gray, red, blue, green, yellow, purple, brown, orange
+color_list = ['#FFF8DC', '#000000', '#A9A9A9', '#DC143C', '#0000CD', '#32CD32', '#FFD700', '#9400D3', '#A0522D', '#FF8C00'];
+
+# Strings can be indexed, so it's all put into a string for now
+alphanumerics = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 for file in os.listdir("svg"):
-    svg_code = open(f"./svg/{file}", "r")
+    shape_color = color_list[r.randint(0,9)]
+    while True:
+        text_color = color_list[r.randint(0,9)]
+        if text_color != shape_color:
+            break
 
-    svg2png(bytestring=svg_code.read(),write_to=f'./png/{file.replace(".svg","")}.png')
+    svg_code = open(f"./svg/{file}", "r")
+    img = svg_code.read()
+    img = img.replace("A", alphanumerics[ r.randint(0,35) ])
+    img = img.replace('#000000', shape_color, -1)
+    img = img.replace('#FFFFFF', text_color, -1)
+    svg2png(bytestring=img, write_to=f'./png/{file.replace(".svg","")}.png')
