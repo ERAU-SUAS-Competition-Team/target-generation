1d2c238ce3f4dc29b5e89767e6e6c6d0b85997df
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2023-01-18 14:17:15 -0500

    Add file naming

--- test.py
+++ test.py
@@ -12,28 +12,35 @@ color_list = ['#FFF8DC', '#000000', '#A9A9A9', '#DC143C', '#0000CD', '#32CD32', 
 # Strings can be indexed, so it's all put into a string for now
 alphanumerics = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
-for file in os.listdir("svg"):
-    # Check for buffer, delete if it exists
-    if os.path.exists("./buffer.svg"):
-        os.remove("./buffer.svg")
-    
-    shape_color = color_list[r.randint(0,9)]
-    while True:
-        text_color = color_list[r.randint(0,9)]
-        if text_color != shape_color:
-            break
+# Number of images to generate!
+cap = 250
+iteration = 0
 
-    svg_code = open(f"./svg/{file}", "r")
-    img = svg_code.read()
-    img = img.replace("A", alphanumerics[ r.randint(0,35) ])
-    img = img.replace('#000000', shape_color, -1)
-    img = img.replace('#FFFFFF', text_color, -1)
-    
-    out = open(f"./buffer.svg", "w")
-    out.write(img)
-    
-    # Reorganize in order to store the state of the generator, in order to save
-    # metadata for training an object model
-    
-    os.system(f'inkscape --export-type="png" --export-filename="./png/{file.replace(".svg","")}.png" buffer.svg')
-    # svg2png(bytestring=img, write_to=f'./png/{file.replace(".svg","")}.png')
+while iteration < cap:
+    for file in os.listdir("svg"):
+        # Check for buffer, delete if it exists
+        if os.path.exists("./buffer.svg"):
+            os.remove("./buffer.svg")
+        
+        shape_color = color_list[r.randint(0,9)]
+        while True:
+            text_color = color_list[r.randint(0,9)]
+            if text_color != shape_color:
+                break
+
+        svg_code = open(f"./svg/{file}", "r")
+        img = svg_code.read()
+        img = img.replace("A", alphanumerics[ r.randint(0,35) ])
+        img = img.replace('#000000', shape_color, -1)
+        img = img.replace('#FFFFFF', text_color, -1)
+        
+        out = open(f"./buffer.svg", "w")
+        out.write(img)
+        
+        # Reorganize in order to store the state of the generator, in order to save
+        # metadata for training an object model
+        
+        os.system(f'inkscape --export-type="png" --export-filename="./png/{iteration}.png" buffer.svg')
+        
+        iteration += 1
+        # svg2png(bytestring=img, write_to=f'./png/{file.replace(".svg","")}.png')
