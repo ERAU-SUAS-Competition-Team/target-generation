f08fc841b3c912a12a9cf68a37d527eaac051bf9
Thomas Pasfield <111624637+tpasfieldERAU@users.noreply.github.com>
2023-02-24 14:51:43 -0500

    make sat_gen a module, add broader customization

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 """ Main Generation Script
     This will eventually be constructed, but is a palceholder for now.
 """
+
+import cv2 as cv
+import numpy as np
+import os
+
+import sat_gen as sg
+
+SIZE = 60
+
+img = cv.imread('./sat_in/stmary.png')
+
+wid = img.shape[1]
+hei = img.shape[0]
+res = [wid, hei]
+
+tile = sg.genTile(img, res, SIZE)
+
+cv.imshow('tile', tile)
+cv.waitKey()
--- sat_gen.py
+++ sat_gen.py
@@ -5,15 +5,6 @@
      Completely broken lmao
 """
 
-IMAGE_COUNT = 100
-SIZE = 60
-CB_VAR = False
-
-# For diagnostic purposes
-SCALE_OUT = True
-OUT_SIZE = 400
-
-
 import os
 import random as r
 import cv2 as cv
@@ -22,8 +13,9 @@ import numpy as np
 if not os.path.exists('./sat_out/'):
     os.mkdir('./sat_out/')
     
-    
-def genSquare(mat, res, size):
+
+# Generates a single tile from the image, in the default orientation    
+def genTile(mat, res, size):
     img = mat
     x = int(r.randint(0, res[0]-(size + 1)))
     y = int(r.randint(0, res[1]-(size + 1)))    
@@ -31,47 +23,71 @@ def genSquare(mat, res, size):
     return buf
 
 
-img = cv.imread('./sat_in/stmary.png')
-img = cv.cvtColor(img, cv.COLOR_BGR2HSV).astype("float32")
-wid = img.shape[1]
-hei = img.shape[0]
-res = [wid, hei]
-
-n=0
-while n < IMAGE_COUNT:
-    buf = genSquare(img, res, SIZE)
-    
+# Randomly rotates an image by some multiple of 90 degrees
+def randomRotate(mat):
     i = r.randint(0,3)
     if i == 0:
         pass
     elif i == 1:
-        cv.rotate(buf, cv.ROTATE_90_CLOCKWISE)
+        cv.rotate(mat, cv.ROTATE_90_CLOCKWISE)
     elif i == 2:
-        cv.rotate(buf, cv.ROTATE_180)
+        cv.rotate(mat, cv.ROTATE_180)
     elif i == 3:
-        cv.rotate(buf, cv.ROTATE_90_COUNTERCLOCKWISE)
+        cv.rotate(mat, cv.ROTATE_90_COUNTERCLOCKWISE)
+    else:
+        pass
+    return mat
 
+
+# Randomly saturates an image
+def randomSaturation(mat):
+    mat = cv.cvtColor(mat, cv.COLOR_BGR2HSV).astype("float32")
     sat = r.randrange(1, 3)
-    [h, s, v] = cv.split(buf)
+    [h, s, v] = cv.split(mat)
     s = s * sat
     s = np.clip(s, 0, 255)
-    buf = cv.merge([h,s,v])
-    buf = cv.cvtColor(buf.astype("uint8"), cv.COLOR_HSV2BGR)
-    
-    if CB_VAR:
-        temp = np.zeros(buf.shape, buf.dtype)
-        alpha = 0.5 + 1.5 * r.random()
-        beta = r.randint(-10, 10)
-    
-        for y in range(buf.shape[0]):
-            for x in range(buf.shape[1]):
-                for c in range(buf.shape[2]):
-                    temp[y,x,c] = np.clip(alpha*buf[y,x,c] + beta, 0, 255)
-        buf = temp
-    
-    if SCALE_OUT:
-        buf = cv.resize(buf, [OUT_SIZE, OUT_SIZE], cv.INTER_AREA)
-    cv.imwrite('./sat_out/' + str(n) + '.png' , buf)
-    n+=1
+    mat = cv.merge([h,s,v])
+    mat = cv.cvtColor(mat.astype("uint8"), cv.COLOR_HSV2BGR)
+    return mat
 
-print("complete.")
+
+# Randomly changes the contrast of an image
+def randomContrast(mat):
+    temp = np.zeros(mat.shape, mat.dtype)
+    alpha = 0.5 + 1.5 * r.random()
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            for c in range(mat.shape[2]):
+                temp[y,x,c] = np.clip(alpha * mat[y,x,c], 0, 255)
+    return temp
+
+
+# Randomly changes the brightness of an image
+def randomBrightness(mat):
+    temp = np.zeros(mat.shape, mat.dtype)
+    beta = r.randint(-15,15)
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            for c in range(mat.shape[2]):
+                temp[y,x,c] = np.clip(mat[y,x,c] + beta, 0, 255)
+    return temp
+
+
+# Randomly changes both the brightness and the contrast of an image
+def randomCB(mat):
+    temp = np.zeros(mat.shape, mat.dtype)
+    alpha = 0.5 + 1.5 * r.random()
+    beta = r.randint(-15, 15)
+
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            for c in range(mat.shape[2]):
+                temp[y,x,c] = np.clip(alpha*mat[y,x,c] + beta, 0, 255)
+    return temp
+
+
+# Changes the scale of the image with some given input, for diagnostics
+def scale(mat, size):
+    mat = cv.resize(mat, [size,size], cv.INTER_AREA)
+    return mat
+    
